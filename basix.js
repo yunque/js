378807d35292d45a7c34fb28fc62c4a209b19721
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-05-02 01:40:05 +0100

    JavaScript syntax basics

---
+++ basix.js
@@ -0,0 +1,13 @@
+// Open the JavaScript console to debug info, etc.
+Ctrl+Shift+J
+
+// Array
+array = ["a", "b", "c"];
+var a = Array("d", "e", "f");
+
+// Get random element from array
+var randElement = array[Math.floor(Math.random()*array.length)]
+
+// Output
+document.write(array)
+console.log(array)

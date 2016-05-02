ab603656f726197f56de997dd2d00460fb03b73e
IMR <i.martinez08@alumni.imperial.ac.uk>
2016-05-02 16:27:55 +0100

    Array operations: push, pop

--- basix.js
+++ basix.js
@@ -5,6 +5,15 @@ Ctrl+Shift+J
 array = ["a", "b", "c"];
 var a = Array("d", "e", "f");
 
+// Add to array
+array.push("xyz")
+
+// Remove from Array
+array.pop("xyz")
+
+// Get index of array element
+var pos = array.indexOf("a")
+
 // Get random element from array
 var randElement = array[Math.floor(Math.random()*array.length)]
 
